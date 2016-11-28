b8b945b4159546748ed58ebcb2eaf2b04875cc49
comp500 <comp500@users.noreply.github.com>
2016-11-28 15:50:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var BME280 = require('node-bme280');
 var barometer = new BME280({address: 0x77});
+var latestTemp;
+var latestPressure;
+var latestHumidity;
+var ready = false;
 
 var Datastore = require('nedb')
   , db = new Datastore({ filename: 'data.txt', autoload: true });
@@ -12,16 +16,35 @@ barometer.begin(function(err) {
         return;
     }
     console.info('barometer running');
+	setImmediate(readData);
+	setInterval(readData, 60 * 1000);
 });
 
-app.get('/', function (req, res) {
+var readData = function () {
 	barometer.readPressureAndTemparature(function(err, pressure, temperature, humidity) {
-		res.write("<html><head><meta http-equiv=\"refresh\" content=\"5\"><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head><body>");
-		res.write("Temperature: " + temperature.toFixed(2) + " ℃<br>");
-		res.write("Pressure: " + (pressure / 100).toFixed(2) + " hPa<br>");
-		res.write("Humidity: " + humidity.toFixed(2) + "%\n");
-		res.end("</body></html>");
+		latestTemp = temperature.toFixed(2);
+		latestPressure = (pressure / 100).toFixed(2);
+		latestHumidity = humidity.toFixed(2);
+		ready = true;
+		db.insert({
+			time: new Date(),
+			ambientTemperature: latestTemp,
+			pressure: latestPressure,
+			humidity: latestHumidity
+		});
 	});
+};
+
+app.get('/', function (req, res) {
+	res.write("<html><head><meta http-equiv=\"refresh\" content=\"5\"><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head><body>");
+	if (ready) {
+		res.write("Temperature: " + latestTemp + " ℃<br>");
+		res.write("Pressure: " + latestPressure + " hPa<br>");
+		res.write("Humidity: " + latestHumidity + "%\n");
+	} else {
+		res.write("No data is available currently.");
+	}
+	res.end("</body></html>");
 });
 
 app.listen(3000, function () {
