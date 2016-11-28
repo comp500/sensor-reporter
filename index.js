9766e522db52c11f3a6b158ed746bdd9c1eba494
comp500 <comp500@users.noreply.github.com>
2016-11-28 15:51:22 +0000

    change port

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ app.get('/', function (req, res) {
 	res.end("</body></html>");
 });
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+	console.log('Example app listening on port 80!');
 });
