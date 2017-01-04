f58b913ac5d487ea6716a55a881a874d8da70ff1
arbvl <12beavila@gravesendgrammar.eu>
2017-01-04 16:15:00 +0000

    Update graphloader.js

--- static/src/graphloader.js
+++ static/src/graphloader.js
@@ -1,6 +1,56 @@
 $(function() { 
-	$.getJSON( "data.json", function( data ) {
+	$.getJSON( "data.json", function( ajaxdata ) {
     	var items = [];
+		var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		var config = {
+			type: 'line',
+			data: {
+				labels: ["1800", "2000", "2200", "2400", "0200", "0400", "0600", "0800", "1000"],
+				datasets: [{
+					label: "Temperature",
+					backgroundColor: window.chartColors.red,
+					borderColor: window.chartColors.red,
+					data: ajaxdata,					],
+					fill: false,
+				}]
+			},
+			options: {
+				responsive: true,
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Time',
+							fontFamily: "serif"
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Temperature'
+						}
+					}]
+				}
+			}
+		};
+		window.onload = function() {
+			var ctx = document.getElementById("trunting").getContext("2d");
+			window.myLine = new Chart(ctx, config);
+		};
+		window.onload = function() {
+			var ctx = document.getElementById("ambient").getContext("2d");
+			window.myLine = new Chart(ctx, config);
+		};
 		});
   });
 
