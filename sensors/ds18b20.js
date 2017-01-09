b87f6c24fc34056bdf465217c4696a7875b33198
arbvl <12beavila@gravesendgrammar.eu>
2017-01-09 13:28:49 +0000

    wip

--- sensors/ds18b20.js
+++ sensors/ds18b20.js
@@ -1,16 +1,25 @@
 var fs = require("fs");
+var glob = require("glob");
 
 module.exports = function () {
-	return new Promise(function (resolve, reject) {
-		resolve([
-			{
-				sensorID: 4,
-				value: latestTemp,
-				unit: "&#176;C",
-				measurement: "Temperature",
-				location: "Soil",
-				htmlDecimal: 1
-			}
-		]);
+	glob("/sys/bus/w1/devices/28*/w1_slave", function (er, files) {
+		return new Promise(function (resolve, reject) {
+			fs.readFile('/etc/hosts', 'utf8', function (err,data) {
+				if (err) {
+				return console.log(err);
+				}
+				console.log(data);
+			});
+			resolve([
+				{
+					sensorID: 4,
+					value: latestTemp,
+					unit: "&#176;C",
+					measurement: "Temperature",
+					location: "Soil",
+					htmlDecimal: 1
+				}
+			]);
+		});
 	});
 }
