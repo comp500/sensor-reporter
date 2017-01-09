a1ff4b0e7bc0683f17a56077e2d36c9d40b607da
arbvl <12beavila@gravesendgrammar.eu>
2017-01-09 14:59:21 +0000

    Update graphloader.js

--- static/src/graphloader.js
+++ static/src/graphloader.js
@@ -49,3 +49,52 @@ $(function() {
 		});
   });
 
+$(function() { 
+	$.getJSON( "data.json", function( ajaxdatapress ) {
+    	var items = [];
+ 		var config = {
+ 			type: 'line',
+ 			data: {
+ 				labels: ["-95", "-90", "-85", "-80", "-75", "-70", "--65", "-60", "-55", "-50", "-45", "-40", "-35", "-30", "-25", "-20", "-15", "-10", "-5", "0"],
+ 				datasets: [{
+ 					label: "Pressure (&deg;C)",
+ 					backgroundColor: window.chartColors.red,
+ 					borderColor: window.chartColors.red,
+ 					data: ajaxdata,
+ 					fill: false,
+ 				}]
+ 			},
+ 			options: {
+ 				responsive: true,
+ 				tooltips: {
+ 					mode: 'index',
+ 					intersect: false,
+ 				},
+ 				hover: {
+ 				mode: 'nearest',
+ 					intersect: true
+ 				},
+ 				scales: {
+ 				xAxes: [{
+ 						display: true,
+ 						scaleLabel: {
+ 							display: true,
+ 							labelString: 'Time (Mins ago)',
+ 							fontFamily: "sans-serif"
+ 						}
+ 					}],
+ 					yAxes: [{
+ 						display: true,
+ 						scaleLabel: {
+ 						display: true,
+ 							labelString: 'Temperature'
+ 						}
+ 					}]
+ 				}
+ 		}
+ 		};
+
+			var ctx2 = document.getElementById("ambient").getContext("2d");
+ 			window.myLine = new Chart(ctx2, config);
+		});
+  });
