a1196eb85ef69880322c82201648296d8b72b714
arbvl <12beavila@gravesendgrammar.eu>
2017-01-11 15:02:11 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.baroInterval = 60 * 1000; // 1 minute readings for Pressure
+var baroInterval = 60 * 1000; // 1 minute readings for Pressure
 exports.tempInterval = 60 * 1000; // 1 minute readings for Temperature
 exports.humiInterval = 60 * 1000; // 1 minute readings for Humidity
