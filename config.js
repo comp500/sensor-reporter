fd0c14e043dd919516873b2d715d8decc0e129a5
arbvl <12beavila@gravesendgrammar.eu>
2017-01-11 14:34:53 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+var baroInterval = 60 * 1000; // 1 minute readings for Pressure
+var tempInterval = 60 * 1000; // 1 minute readings for Temperature
+var humiInterval = 60 * 1000; // 1 minute readings for Humidity
