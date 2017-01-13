acc3ffe9894d5c844c40da986ebdae3fc10f1c85
comp500 <comp500@users.noreply.github.com>
2017-01-13 08:50:18 +0000

    Update index.js

--- sensors/index.js
+++ sensors/index.js
@@ -1,11 +1,13 @@
 var optional = require("optional");
-
-var promises = [
-	optional("./bme280.js"),
-	optional("./ds18b20.js")
-];
+var promises;
 
 module.exports = {};
 module.exports.load = new Promise(function (resolve, reject) {
-	
+	process.nextTick(function () {
+		promises = [
+			optional("./bme280.js"),
+			optional("./ds18b20.js")
+		];
+		resolve();
+	});
 });
