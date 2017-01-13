eb7149794e9f818dd6bd1ea90191fc211b45d283
comp500 <comp500@users.noreply.github.com>
2017-01-13 15:01:21 +0000

    added config settings

--- config.js
+++ config.js
@@ -1,5 +1,17 @@
 module.exports = {
 	baroInterval: 60 * 1000, // 1 minute readings for Pressure
 	tempInterval: 60 * 1000, // 1 minute readings for Temperature
-	humiInterval: 60 * 1000 // 1 minute readings for Humidity
+	humiInterval: 60 * 1000, // 1 minute readings for Humidity
+	ambientTemperature: {
+		graphDecimal: 1,
+		exportDecimal: 1
+	},
+	pressure: {
+		graphDecimal: 0,
+		exportDecimal: 0
+	},
+	humidity: {
+		graphDecimal: 0,
+		exportDecimal: 0
+	},
 };
