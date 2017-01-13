4a3545a8c2e58131fe7e35114c35113dbca61c28
comp500 <comp500@users.noreply.github.com>
2017-01-13 15:23:17 +0000

    move interval to objects

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-	baroInterval: 60 * 1000, // 1 minute readings for Pressure
-	tempInterval: 60 * 1000, // 1 minute readings for Temperature
-	humiInterval: 60 * 1000, // 1 minute readings for Humidity
 	ambientTemperature: {
 		graphDecimal: 1,
-		exportDecimal: 1
+		exportDecimal: 1,
+		interval: 60 * 1000 // 1 minute readings for Temperature
 	},
 	pressure: {
 		graphDecimal: 0,
-		exportDecimal: 0
+		exportDecimal: 0,
+		interval: 60 * 1000 // 1 minute readings for Pressure
 	},
 	humidity: {
 		graphDecimal: 0,
-		exportDecimal: 0
+		exportDecimal: 0,
+		interval: 60 * 1000 // 1 minute readings for Humidity
 	},
 };
