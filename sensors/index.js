6f5097e51d4fc36f598703b64786e55c9b1498af
comp500 <comp500@users.noreply.github.com>
2017-01-16 10:41:29 +0000

    functions for promises

--- sensors/index.js
+++ sensors/index.js
@@ -2,24 +2,28 @@ var optional = require("optional");
 var promises;
 
 module.exports = {};
-module.exports.load = new Promise(function (resolve, reject) {
-	process.nextTick(function () {
-		promises = [
-			optional("./bme280.js"),
-			optional("./ds18b20.js")
-		];
-		resolve();
+module.exports.load = function () {
+	return new Promise(function (resolve, reject) {
+		process.nextTick(function () {
+			promises = [
+				optional("./bme280.js"),
+				optional("./ds18b20.js")
+			];
+			resolve();
+		});
 	});
 });
 
-module.exports.run = new Promise(function (resolve, reject) {
-	Promise.all(promises).then(function (values) {
-		var sensors = [];
-		for (var i = 0; i < values.length; i++) {
-			sensors.concat(values[i]);
-		}
-		resolve(sensors);
-	}).catch(function (reason) {
-		reject(reason);
+module.exports.run = function () {
+	return new Promise(function (resolve, reject) {
+		Promise.all(promises).then(function (values) {
+			var sensors = [];
+			for (var i = 0; i < values.length; i++) {
+				sensors.concat(values[i]);
+			}
+			resolve(sensors);
+		}).catch(function (reason) {
+			reject(reason);
+		});
 	});
 });
