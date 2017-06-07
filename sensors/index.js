e9a797ebd4942d47ddbe7c24da79717192b348d8
comp500 <comp500@users.noreply.github.com>
2017-06-07 14:31:12 +0100

    add comments and simplify

--- sensors/index.js
+++ sensors/index.js
@@ -2,41 +2,38 @@ var optional = require("optional");
 var promises;
 
 module.exports = {};
-module.exports.load = function () {
+module.exports.load = function () { // load all modules
 	return new Promise(function (resolve, reject) {
-		process.nextTick(function () {
-			promises = [
+		process.nextTick(function () { // wait for some reason
+			promises = [ // list of all modules to be loaded
 				optional("./bme280.js"),
-//				optional("./ds18b20.js")
+//				optional("./ds18b20.js"),
 				optional("./tsl2591.js")
 			];
-			var calledPromises = [];
-			for (var i = 0; i < promises.length; i++) {
-				calledPromises[i] = promises[i].load();
+			var calledPromises = []; // array of promise callbacks
+			for (var i = 0; i < promises.length; i++) { // loop through all modules
+				calledPromises[i] = promises[i].load(); // run all modules
 			}
-			Promise.all(calledPromises).then(function (values) {
-				resolve();
-			}).catch(function (reason) {
-				reject(reason);
-			});
+			// resolve once all promises are called
+			resolve(Promise.all(calledPromises));
 		});
 	});
 };
 
-module.exports.run = function () {
+module.exports.run = function () { // run all modules
 	return new Promise(function (resolve, reject) {
-		var calledPromises = [];
-		for (var i = 0; i < promises.length; i++) {
-			calledPromises[i] = promises[i].getData();
+		var calledPromises = []; // array of promise callbacks
+		for (var i = 0; i < promises.length; i++) { // loop through all modules
+			calledPromises[i] = promises[i].getData(); // run all modules
 		}
 		Promise.all(calledPromises).then(function (values) {
-			var sensors = [];
+			var sensors = []; // array for sensor values
 			for (var i = 0; i < values.length; i++) {
-				sensors = sensors.concat(values[i]);
+				sensors = sensors.concat(values[i]); // add callback values to array
 			}
-			resolve(sensors);
+			resolve(sensors); // resolve array
 		}).catch(function (reason) {
-			reject(reason);
+			reject(reason); // reject if error
 		});
 	});
 };
