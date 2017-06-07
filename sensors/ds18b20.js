fa86807591d313befbb60158affb1740de75161b
comp500 <comp500@users.noreply.github.com>
2017-06-07 12:13:10 +0100

    rewrite to use promises properly

--- sensors/ds18b20.js
+++ sensors/ds18b20.js
@@ -1,29 +1,46 @@
 var fs = require("fs");
 var glob = require("glob");
+var fileName = "";
 
-module.exports = function () {
-	glob("/sys/bus/w1/devices/28*/w1_slave", function (er, files) {
-		return new Promise(function (resolve, reject) {
+module.exports = {};
+
+module.exports.load = function () {
+	return new Promise(function (resolve, reject) {
+		glob("/sys/bus/w1/devices/28*/w1_slave", function (er, files) {
 			if (files.length < 1) {
 				reject("Device not found. Try starting up the kernel modules.");
+			} else {
+				fileName = files[0];
+				if (files.length > 1) {
+					console.info("Multiple devices found. Readings may be incorrect.");
+				}
+				resolve();
 			}
-			fs.readFile(files[0], 'utf8', function (err,data) {
-				if (err) {
-				return console.log(err);
-				}
-				console.log(data);
-			});
-			resolve([
-				{
-					sensorID: 4,
-					value: latestTemp,
-					unit: "&#176;C",
-					measurement: "Temperature",
-					location: "Soil",
-					htmlDecimal: 1
-				}
-			]);
 		});
 	});
-}
-// im a bad programmer
+};
+
+module.exports.getData = function () {
+	return new Promise(function (resolve, reject) {
+		if (fileName == null) {
+			reject("fileName not loaded");
+		}
+		fs.readFile(fileName, 'utf8', function (err, data) {
+			if (err) {
+			return console.log(err);
+			}
+			console.log(data);
+		});
+		// TODO: Parse file
+		/*resolve([
+			{
+				sensorID: 4,
+				value: latestTemp,
+				unit: "&#176;C",
+				measurement: "Temperature",
+				location: "Soil",
+				htmlDecimal: 1
+			}
+		]);*/
+	});
+};
