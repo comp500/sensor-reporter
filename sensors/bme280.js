580a18ae2a01c438393a00c75e0ac3c2621d870c
comp500 <comp500@users.noreply.github.com>
2017-06-07 12:03:23 +0100

    rewrite sensor api

--- sensors/bme280.js
+++ sensors/bme280.js
@@ -1,7 +1,9 @@
 var BME280 = require('node-bme280');
 var barometer = new BME280({address: 0x77});
 
-module.exports = function () {
+module.exports = {};
+
+module.exports.load = function () {
 	return new Promise(function (resolve, reject) {
 		barometer.begin(function(err) {
 			if (err) {
@@ -9,42 +11,46 @@ module.exports = function () {
 				reject();
 			} else {
 				console.info('barometer running');
-				resolve(new Promise(function (resolve, reject) {
-					barometer.readPressureAndTemparature(function(err, pressure, temperature, humidity) {
-						latestTemp = temperature.toFixed(2);
-						latestPressure = (pressure / 100).toFixed(2);
-						latestHumidity = humidity.toFixed(2);
-						resolve([
-							{
-								sensorID: 1,
-								value: latestTemp,
-								unit: "&#176;C",
-								measurement: "Temperature",
-								location: "ICT office",
-								htmlDecimal: 1
-							},
-							{
-								sensorID: 2,
-								value: latestHumidity,
-								unit: "%",
-								measurement: "Humidity",
-								location: "ICT office",
-								htmlDecimal: 1
-							},
-							{
-								sensorID: 3,
-								value: latestPressure,
-								unit: "hPa",
-								small: true,
-								measurement: "Pressure",
-								location: "ICT office",
-								htmlDecimal: 0
-							}
-						]);
-						//resolve();
-					});
-				}));
+				resolve();
 			}
 		});
 	});
 }
+
+module.exports.getData = function () {
+	resolve(new Promise(function (resolve, reject) {
+		barometer.readPressureAndTemparature(function(err, pressure, temperature, humidity) {
+			latestTemp = temperature.toFixed(2);
+			latestPressure = (pressure / 100).toFixed(2);
+			latestHumidity = humidity.toFixed(2);
+			resolve([
+				{
+					sensorID: 1,
+					value: latestTemp,
+					unit: "&#176;C",
+					measurement: "Temperature",
+					location: "ICT office",
+					htmlDecimal: 1
+				},
+				{
+					sensorID: 2,
+					value: latestHumidity,
+					unit: "%",
+					measurement: "Humidity",
+					location: "ICT office",
+					htmlDecimal: 1
+				},
+				{
+					sensorID: 3,
+					value: latestPressure,
+					unit: "hPa",
+					small: true,
+					measurement: "Pressure",
+					location: "ICT office",
+					htmlDecimal: 0
+				}
+			]);
+			//resolve();
+		});
+	});
+};
