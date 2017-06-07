e327d19c59739b6836e349e7a1537e694d2ddeb9
comp500 <comp500@users.noreply.github.com>
2017-06-07 13:53:14 +0100

    add light sensor

--- sensors/index.js
+++ sensors/index.js
@@ -6,8 +6,9 @@ module.exports.load = function () {
 	return new Promise(function (resolve, reject) {
 		process.nextTick(function () {
 			promises = [
-				optional("./bme280.js")
+				optional("./bme280.js"),
 //				optional("./ds18b20.js")
+				optional("./tsl2591.js")
 			];
 			var calledPromises = [];
 			for (var i = 0; i < promises.length; i++) {
