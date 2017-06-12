f75b53fa700c0d7f78a7c04d7cb8a37200a45250
comp500 <comp500@users.noreply.github.com>
2017-06-12 14:01:03 +0100

    implement functions

--- databases/nedb.js
+++ databases/nedb.js
@@ -15,17 +15,49 @@ module.exports.connect = function () { // connect to database
 };
 
 module.exports.pushData = function (values) { // add new data, as JSON
-	
+	return new Promise(function (resolve, reject) {
+		db.insert(values, function (err, newDoc) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(newDoc);
+			}
+		});
+	});
 };
 
 module.exports.getGraphs = function () { // get graphs TODO: specify time/date
-	
+	return new Promise(function (resolve, reject) {
+		db.find({}).sort({ time: -1 }).limit(100).exec(function (err, docs) { // query 100 newest entries, newest first
+			if (err) {
+				reject(err);
+			} else {
+				resolve(docs.reverse());
+			}
+		});
+	});
 };
 
 module.exports.getExportAll = function () { // get all data in database
-	
+	return new Promise(function (resolve, reject) {
+		db.find({}).sort({ time: 1 }).exec(function (err, docs) { // find all records and sort
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	});
 };
 
 module.exports.getExportBetweenDates = function (dateStart, dateEnd) { // get data between dates
-	
+	return new Promise(function (resolve, reject) { // TODO filter dates
+		db.find({}).sort({ time: 1 }).exec(function (err, docs) { // find all records and sort
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	});
 };
