196b62c2534e4b37042a3602a6986e665d7f54ca
comp500 <comp500@users.noreply.github.com>
2017-06-12 13:56:47 +0100

    fix brackets

--- databases/nedb.js
+++ databases/nedb.js
@@ -2,7 +2,7 @@ var Datastore = require('nedb');
 var db = new Datastore({ filename: 'latest2.txt' });
 
 module.exports = {};
-module.exports.connect(function () { // connect to database
+module.exports.connect = function () { // connect to database
 	return new Promise(function (resolve, reject) {
 		db.loadDatabase(function (err) {
 			if (err) {
@@ -12,20 +12,20 @@ module.exports.connect(function () { // connect to database
 			}
 		});
 	});
-});
+};
 
-module.exports.pushData(function (values) { // add new data, as JSON
+module.exports.pushData = function (values) { // add new data, as JSON
 	
-});
+};
 
-module.exports.getGraphs(function () { // get graphs TODO: specify time/date
+module.exports.getGraphs = function () { // get graphs TODO: specify time/date
 	
-});
+};
 
-module.exports.getExportAll(function () { // get all data in database
+module.exports.getExportAll = function () { // get all data in database
 	
-});
+};
 
-module.exports.getExportBetweenDates(function (dateStart, dateEnd) { // get data between dates
+module.exports.getExportBetweenDates = function (dateStart, dateEnd) { // get data between dates
 	
-});
+};
