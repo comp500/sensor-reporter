d944ef00943c202036db132733d346663761292b
comp500 <comp500@users.noreply.github.com>
2018-10-04 17:41:25 +0100

    Update bme280 to new api

--- sensors/bme280.js
+++ sensors/bme280.js
@@ -1,36 +1,25 @@
-var BME280 = require('node-bme280');
-var barometer = new BME280({address: 0x77});
+const BME280 = require('bme280-sensor');
+const bme280 = new BME280(options);
 
 module.exports = {};
 
 module.exports.load = function () {
-	return new Promise(function (resolve, reject) {
-		barometer.begin(function(err) {
-			if (err) {
-				console.info('error initializing barometer', err);
-				reject();
-			} else {
-				console.info('barometer running');
-				resolve();
-			}
-		});
-	});
+	return bme280.init();
 }
 
 module.exports.getData = function () {
 	return new Promise(function (resolve, reject) {
-		barometer.readPressureAndTemparature(function(err, pressure, temperature, humidity) {
-			if (err) {
-				reject(err);
-			}
-			var latestTemp = temperature.toFixed(2); // TODO: make this use config file for decimal places?
-			var latestPressure = (pressure / 100).toFixed(2);
-			var latestHumidity = humidity.toFixed(2);
+		bme280.readSensorData().then((data) => {
+			var latestTemp = data.temperature_C.toFixed(2); // TODO: make this use config file for decimal places?
+			var latestPressure = (data.pressure_hPa / 100).toFixed(2);
+			var latestHumidity = data.humidity.toFixed(2);
 			resolve({
 				0: latestTemp,
 				1: latestHumidity,
 				2: latestPressure
 			});
+		}).catch((err) => {
+			reject(err);
 		});
 	});
 };
